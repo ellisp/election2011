141bff477b3c47dd560f0df48ac7921a5bf9d452
ellisp <peter.ellis2013nz@gmail.com>
2014-03-09 10:13:16 +1300

    add centres to electoral boundaries map and change folder structure etc to be more rerpdocueable

--- grooming_code/import_electorate_maps.R
+++ grooming_code/import_electorate_maps.R
@@ -1,12 +1,25 @@
 library(sp)
 library(rgdal)
+library(mbiemaps) # for AddCoord
 
 
-path <- "C:/users/peter ellis/documents/shapefiles/New Zealand 2007 Electoral Districts"
+download.file("https://www3.stats.govt.nz/digitalboundaries/annual/New%20Zealand%202007%20Electoral%20Districts%20(NZTM)%20(8.35MB).zip",
+              "raw_data/shapefiles/electoral_boundaries.zip")
+
+path <- "raw_data/shapefiles/New Zealand 2007 Electoral Districts"
+
+unzip("raw_data/shapefiles/electoral_boundaries.zip", exdir="raw_data/shapefiles")
+
 
 
 electorates_map <- readOGR(dsn=path, layer="ged07")
-electorates_map_gg <- fortify(electorates_map, region="NAME")
+electorates_map@data <- AddCoord(electorates_map)
+tmp <- fortify(electorates_map, region="NAME")
+names(tmp) <- gsub("id", "NAME", names(tmp))
+tmp2 <- merge(tmp, electorates_map@data, suffixes=c("", ".centre"), by="NAME")
+electorates_map_gg <- tmp2
 
 save(electorates_map, file="pkg/data/electorates_map.rda")
 save(electorates_map_gg, file="pkg/data/electorates_map_simpl_gg.rda")
+
+rm(tmp, tmp2, path)
