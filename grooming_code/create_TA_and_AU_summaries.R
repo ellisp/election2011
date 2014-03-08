84405691eb569b565a0f72ab01891931f7fb41d2
ellisp <peter.ellis2013nz@gmail.com>
2014-03-09 11:38:49 +1300

    Merge TA data frame with a whole bunch of census information

---
+++ grooming_code/create_TA_and_AU_summaries.R
@@ -0,0 +1,111 @@
+library(census2013)
+library(mbiemaps)
+data(TA)
+data(AU)
+
+#==============aggregate up by TA and by area unit===================
+
+byArea <- merge(party_results_polling_place, Polling_Place_latlong@data, 
+              by.x="Polling_Place", by.y="Address", all.x=TRUE) 
+
+
+#--------------------TA-----------------------
+# Aggregate vote by TA
+byTA <- ddply(byArea, .(TA_fullname, TA, Party), summarise,
+              Votes = sum(Votes))
+
+# Change the NAs to 'Unknown or overseas'
+byTA$TA_fullname <- as.character(byTA$TA_fullname)
+byTA[is.na(byTA)] <- "Unknown or overseas"
+
+# Calculate the total votes (including informal) for later use
+TA_totals <- ddply(byTA, .(TA), summarise, Total_Votes = sum(Votes))
+
+# Transform so you have the percentage vote within each TA
+byTA <- ddply(byTA, .(TA_fullname, TA), mutate, 
+              Percentage = Votes / sum(Votes))
+
+# Cast into wide form - one column per Party
+byTA <- dcast(byTA, TA_fullname + TA ~ Party, sum, value.var="Percentage")
+
+# Add back in the total vote as an extra column
+byTA <- merge(byTA, TA_totals, all=TRUE)
+
+# Add the various information (area, length, centre coordinates) from the shapefile
+byTA <- merge(byTA, TA@data, by.x="TA_fullname", by.y="NAME", all.x=TRUE)
+
+
+# AU
+byAU <- ddply(byArea, .(AU, Party), summarise,
+              Votes = sum(Votes))
+byAU$AU <- as.character(byAU$AU)
+byAU[is.na(byAU)] <- "Unknown or overseas"
+
+AU_totals <- ddply(byAU, .(AU), summarise, Total_Votes = sum(Votes))
+
+byAU <- ddply(byAU, .(AU), mutate, 
+              Percentage = Votes / sum(Votes))
+byAU <- dcast(byAU, AU ~ Party, sum, value.var="Percentage")
+
+byAU <- merge(byAU, AU_totals, all=TRUE)
+
+
+
+byAU <- merge(byAU, AU@data, by.x="AU", by.y="NAME", all.x=TRUE)
+
+
+#===================combine with a selection of census data==================
+
+
+#----------------collate census data----------------------
+# View(TableVariables)
+
+
+# Comment - not clear why this data has 3.7m people as the Total people total ages in total new zealand
+census1 <- ddply(subset(C01Pop_T1, Age=="Total"), .(Geography), summarise,
+      Proportion_Maori_People = sum(Total_People[Ethnicity == "Maori"]) / Total_People[Ethnicity == "Total People"])
+
+
+census2 <- subset(C01Income_T1, Ethnicity == "Total People" & Age_group =="Total")
+census2 <- census2[ , c("Geography", "Median_Personal_Income_Dollars", "Mean_Personal_Income_Dollars")]
+
+# warning - think this is the one with the mean and median transposed hence we swap them roung
+census3 <- subset(Cincome_T4, Ethnicity == "Total People")
+census3 <- census3[ , c("Geography", "Median_Household_Income_Dollars", "Mean_Household_Income_Dollars")]
+names(census3) <- c("Geography", "Mean_Household_Income_Dollars", "Median_Household_Income_Dollars")
+
+census4 <- subset(CWork_T3, Ethnicity == "Total People")
+census4 <- census4[ , c("Geography", "Unemployment_Rate_Percent", "Unemployed")]
+
+census5 <- ddply(subset(CEducation_T1, Age_Group=="Total" & Ethnicity == "Total People"), .(Geography), summarise,
+                 Proportion_No_Education = sum(Total_People[Level_of_education == "None"]) / 
+                   Total_People[Level_of_education == "Total"],
+                 Proportion_Higher_Education = sum(Total_People[Level_of_education == "Level 7/Bachelors and above"]) / 
+                   Total_People[Level_of_education == "Total"])
+
+census_combined <- merge(census1, census2, by="Geography")
+census_combined <- merge(census_combined, census3, by="Geography")
+census_combined <- merge(census_combined, census4, by="Geography")
+census_combined <- merge(census_combined, census5, by="Geography")
+
+rm(census1, census2, census3, census4, census5)
+
+first_space <- substring(census_combined$Geography)
+census_combined_ta <- subset(census_combined, substring(census_combined$Geography, 4, 4) == " ")
+nc <- nchar(as.character(census_combined_ta$Geography))
+census_combined_ta$TA_fullname <- substring(census_combined_ta$Geography, 5, nc)
+
+
+# add back in Auckland
+census_combined_ta <- rbind(census_combined_ta, 
+                            data.frame(subset(census_combined, Geography == "02 Auckland Region"), TA_fullname = "Auckland"))
+
+#-------------------------merge with TA information-----------------------
+byTA <- merge(byTA, census_combined_ta, by = "TA_fullname", all.x=TRUE)
+
+#===================save======================
+
+party_vote_by_TA <- byTA
+party_vote_by_AU <- byAU
+save(party_vote_by_TA, file="pkg/data/party_vote_by_TA.rda")
+save(party_vote_by_AU, file="pkg/data/party_vote_by_AU.rda")
--- grooming_code/import_polling_place_coordinates.R
+++ grooming_code/import_polling_place_coordinates.R
@@ -12,8 +12,7 @@ Polling_Place_coords <- subset(Polling_Place_coords, !is.na(NZTM2000.Easting))
 
 names(Polling_Place_coords) <- gsub("NZTM2000.", "", names(Polling_Place_coords), fixed=TRUE)
 names(Polling_Place_coords) <- gsub("Polling.Place.", "", names(Polling_Place_coords), fixed=TRUE)
-names(Polling_Place_coords)[names(Polling_Place_coords)=="Electorate.Name"] <- "Electorate"
-
+Polling_Place_coords$Electorate.Name <- NULL
 
 p4s <- "+proj=tmerc +lat_0=0.0 +lon_0=173.0 +k=0.9996 +x_0=1600000.0 +y_0=10000000.0 +datum=WGS84 +units=m"
 tmp <- proj4::project(with(Polling_Place_coords, cbind(Easting, Northing)), 
@@ -22,16 +21,18 @@ tmp <- proj4::project(with(Polling_Place_coords, cbind(Easting, Northing)),
 
 Polling_Place_latlong <- SpatialPointsDataFrame(coords =  tmp, 
                                                 proj4string = TA@proj4string, 
-                                                data = Polling_Place_coords[, c("Electorate", "Suburb", "Address")])
+                                                data = Polling_Place_coords[, c("Suburb", "Address")])
 
 
 Polling_Place_latlong@data$TA_fullname <- over(Polling_Place_latlong, TA)$NAME
-Polling_Place_latlong@data$TA <- gsub(" District", "", Polling_Place_latlong@data$TA_fullname)
-Polling_Place_latlong@data$TA <- gsub(" City", "", Polling_Place_latlong@data$TA)
 Polling_Place_latlong@data$AU <- over(Polling_Place_latlong, AU)$NAME
 
 Polling_Place_latlong@data$AU[Polling_Place_latlong@data$Suburb == "Great Barrier Island"] <- "Great Barrier Island"
-Polling_Place_latlong@data$TA[Polling_Place_latlong@data$Suburb == "Great Barrier Island"] <- "Auckland"
+Polling_Place_latlong@data$TA_fullname[Polling_Place_latlong@data$Suburb == "Great Barrier Island"] <- "Auckland"
+
+Polling_Place_latlong@data$TA <- gsub(" District", "", Polling_Place_latlong@data$TA_fullname)
+Polling_Place_latlong@data$TA <- gsub(" City", "", Polling_Place_latlong@data$TA)
+
 
 save(Polling_Place_coords, file="pkg/data/Polling_Place_coords.rda")
 save(Polling_Place_latlong, file="pkg/data/Polling_Place_latlong.rda")
