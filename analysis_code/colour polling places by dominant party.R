212c09096f1b98884f685893bf78f590ccc3c3ea
ellisp <peter.ellis2013nz@gmail.com>
2014-03-08 20:31:20 +1300

    fix issue with colours and google maps

--- analysis_code/colour polling places by dominant party.R
+++ analysis_code/colour polling places by dominant party.R
@@ -7,36 +7,37 @@ elects <- unique(party_results_polling_place$Electorate)
 latlong_df <- data.frame(Polling_Place_latlong@data$Address, Polling_Place_latlong@coords)
 names(latlong_df) <- c("Address", "long", "lat")
 
+
+poll <- subset(party_results_polling_place,  
+               Polling_Place != "Polling places where less than 6 votes were taken" &
+               Party != "Informal Party Votes")
+
+dominant <- ddply(poll, .(Polling_Place, Electorate), summarise,
+                  Biggest=Party[Votes==max(Votes)])
+vol <- ddply(poll, .(Polling_Place), summarise,
+             Volume = sum(Votes))
+dominant <- merge(dominant, vol)
+dominant <- merge(dominant, latlong_df, 
+                  by.x="Polling_Place", by.y="Address", all.x=FALSE)
+
+table(dominant$Biggest)
+
 for (i in 1: length(elects)){
   elect <- elects[i]
   
-  poll <- subset(party_results_polling_place,  Electorate == elect &
-                   Polling_Place != "Polling places where less than 6 votes were taken" &
-                   Party != "Informal Party Votes")
+  # just the electorate:
+  tmp <- subset(dominant, Electorate == elect)
   
-  dominant <- ddply(poll, .(Polling_Place), summarise,
-                    Biggest=Party[Votes==max(Votes)])
-  
-  vol <- ddply(poll, .(Polling_Place), summarise,
-                    Volume = sum(Votes))
-  dominant <- merge(dominant, vol)
-  
-  tmp <- merge(dominant, latlong_df, 
-               by.x="Polling_Place", by.y="Address", all.x=FALSE)
-  
-  # To centre the map so all the coordinates included:
-  # this_area <- get_map(location=c(long=mean(range(tmp$long)), lat=mean(range(tmp$lat))), zoom=9)
-  
-  # to centre it on the mass of the data:
+  # to centre it on the centre of the electorate:
   this_area <- get_map(location=c(long=mean(tmp$long), lat=mean(tmp$lat)), zoom=11, maptype = 'terrain')
   
   
-  png(paste0("output/maps1/map of dominant party by polling place ", elect, ".png"), 6000, 4000, res=500)
+  png(paste0("output/maps1/map of dominant party by polling place centred in ", elect, ".png"), 6000, 4000, res=500)
   print(ggmap(this_area) +
-    geom_point(data=tmp, aes(x=long, y=lat, color=Biggest, size=Volume), alpha=0.3) +
-    geom_point(data=tmp, aes(x=long, y=lat, color=Biggest, size=Volume), pch=1) +
+    geom_point(data=dominant, aes(x=long, y=lat, color=Biggest, size=Volume), alpha=0.3) +
+    geom_point(data=dominant, aes(x=long, y=lat, color=Biggest, size=Volume), pch=1) +
     scale_color_manual(values=party_colours) +
-    ggtitle(paste0("Dominant party by polling location for ", elect, " in 2011 election")) +
+    ggtitle(paste0("Dominant party by polling location centred in ", elect, " in 2011 election")) +
     labs(size="Total valid\nparty votes"))
   dev.off()
       
--- pkg/R/colours.R
+++ pkg/R/colours.R
@@ -1,6 +1,7 @@
 party_colours <- c("Green Party" = "darkgreen", 
                    "Labour Party" = "red", 
-                   "MÄor iParty"  = "Orange", 
                    "Mana" = "black", 
                    "National Party" ="darkblue", 
-                   "New Zealand First Party" = "brown")
+                   "New Zealand First Party" = "brown",
+                   "Aotearoa Legalise Cannabis Party" = "white",
+                   "MÄori Party" = "Orange")
